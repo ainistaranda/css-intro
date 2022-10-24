69f6dc0c816999cef906c8db1cd9a0583647d65f
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-24 15:50:45 -0400

    final, normalized and made classes

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const family = [
+    {   dad: true,
+        name:'bob',
+        job: 'mechanic'
+    },
+    {
+        kid1: 'Tom',
+        pet: 'fish'
+    }
+    
+]
+console.log('hey class here is my fam', family)
+
+document.getElementById('contact').style.color = 'purple'
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+    alert('Alerting the class')
+}
